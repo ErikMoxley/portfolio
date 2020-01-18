d29faae9d45a9668bec055a4d9cfa3e37365396e
ErickMoxley <exilerzero@yahoo.com>
2020-01-18 03:50:53 -0500

    Updates

---
+++ precache-manifest.ed46acc085ccd90517ea0c1e6c2ab895.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e9a473659dd29850f451f737ddeb1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0f602920a1bb26205cb",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5a747b251802e9dfc125",
+    "url": "/static/css/main.cb227b48.chunk.css"
+  },
+  {
+    "revision": "a0f602920a1bb26205cb",
+    "url": "/static/js/2.9387a2a7.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.9387a2a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a747b251802e9dfc125",
+    "url": "/static/js/main.8460379a.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "a293340e7b2e37855586ece3a5ac10b3",
+    "url": "/static/media/background.a293340e.jpg"
+  }
+]);
