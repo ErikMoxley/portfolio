be93bd208215a966b926e1b5bcf040e2076a5586
ErickMoxley <exilerzero@yahoo.com>
2020-01-19 06:06:22 -0500

    update

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
         <NavigationBar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
       </div>
       <div className="container">
