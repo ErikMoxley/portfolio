a199e886a2dd00813f126f6b32dc448abbd24528
ErickMoxley <exilerzero@yahoo.com>
2020-01-19 05:46:23 -0500

    Updates

---
+++ precache-manifest.abe6521d7990a060d1687f021d72dfd3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03250183b723d471ad5883a5c273fb89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea875e24d94b7fb6920b",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2edd9ecb5bd81a7f5915",
+    "url": "/static/css/main.e0582da0.chunk.css"
+  },
+  {
+    "revision": "ea875e24d94b7fb6920b",
+    "url": "/static/js/2.41b45c40.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/static/js/2.41b45c40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2edd9ecb5bd81a7f5915",
+    "url": "/static/js/main.02500685.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "eb6a6cb0becad4a78d6dd1d1f8231ed2",
+    "url": "/static/media/Resume2020.eb6a6cb0.pdf"
+  },
+  {
+    "revision": "a293340e7b2e37855586ece3a5ac10b3",
+    "url": "/static/media/background.a293340e.jpg"
+  },
+  {
+    "revision": "8581ce939e5611f8a7ab36ec1cfc7859",
+    "url": "/static/media/er.8581ce93.jpg"
+  },
+  {
+    "revision": "724794164fb289dd2f7d69dde7ac3bc0",
+    "url": "/static/media/gh.72479416.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91836c0d6a7f8dc5d73925eb4a1ddbc6.js"
+  "/precache-manifest.abe6521d7990a060d1687f021d72dfd3.js"
 );
 
 self.addEventListener('message', (event) => {
